371392a7a8a34a128ff67ea97bc3c49eab526393
Dejurniy <gamesrandom68@gmail.com>
2022-10-30 15:00:08 +0400

    class homework

---
+++ class.homework1.py
@@ -0,0 +1,54 @@
+class Dict:
+
+    def __init__(self, string, dict, dict_2):
+        self.string = string
+        self.dict = dict
+        self.dict_2 = dict_2
+
+    # First method
+    def convert(self):
+
+        """
+        This method creates dictionary from a given string
+        """
+
+        for cr in self.string:
+            if cr not in self.dict:
+                self.dict[cr] = 1
+
+            else:
+                self.dict[cr] += 1
+
+        print(self.dict)
+
+    # Second method
+    def no_dups(self):
+
+        """
+        This method returns dictionary without duplicates
+        """
+
+        for key, value in self.dict.items():
+            if value not in self.dict_2.values():
+                self.dict_2[key] = value
+
+        print(self.dict_2)
+
+    def highest(self):
+
+        """
+        This method returns the highest 3 values in a dictionary!
+        """
+
+        for key, value in self.dict.items():
+            if value > 5:
+                print(key)
+
+
+first_mth = Dict("python", {}, {})
+second_mth = Dict("", {"key1": 1, "key2": 2, "key3": 1}, {})
+third_mth = Dict("", {"key1": 1, "key2": 2, "key3": 10, "key4": 14, "key5": 18}, {})
+
+Dict.convert(first_mth)
+Dict.no_dups(second_mth)
+Dict.highest(third_mth)
---
+++ class.py
@@ -0,0 +1,123 @@
+# class Car:
+#     def __init__(self, name, year, color):
+#         self.car_name = name
+#         self.car_year = year
+#         self.car_color = color
+#
+#
+#     def present(self):
+#         print(f"This {self.car_color} {self.car_name} produced in {self.car_year}")
+#
+#
+#     def get_age(self, current_year):
+#
+#         return current_year - self.car_year
+# #
+# #
+# #
+# #
+# #
+# #
+# car_1 = Car(name="Jaguar", year="2022", color="Black")
+# car_2 = Car(name="Mercedes", year="2022", color="White")
+# car_3 = Car(name="BMW", year="2022", color="Violet")
+# #
+#
+# print(car_1.car_name, car_1.car_year, car_1.car_color)
+# print(car_2.car_name, car_2.car_year, car_2.car_color)
+#
+# Car.present(car_1)
+# print(car_1.get_age(current_year=2022))
+
+
+# class CV:
+#     description = "This is a car class"
+#
+#     def __init__(self, name, surname, email, age):
+#         self.name = name
+#         self.surname = surname
+#         self.email = email
+#         self.age = age
+#
+#     # def present(self):
+#     #     print(f"This is {self.}")
+#
+#
+# user1 = CV(name="Arthur,", surname="Demirchyan,", email="gamesrandom68@gmail.com,", age="18")
+#
+# print(user1.name, user1.surname, user1.email, user1.age)
+
+# class Rectangle:
+#
+#     def __init__(self, a, b):
+#         self.a = a
+#         self.b = b
+#
+#     def perimeter(self):
+#         return (self.a * 2) + (self.b * 2)
+#
+#     def area(self):
+#         return self.a * self.b
+#
+#
+# per = Rectangle(a=4, b=5)
+# print(per.perimeter())
+# print(per.area())
+
+# class Actor:
+#
+#     def __init__(self, name, surname, age):
+#         self.name = name
+#         self.surname = surname
+#         self.age = age
+#
+#     def best_one(self):
+#         print(f"The best actor is {self.name} {self.surname}, who is {self.age} years old!")
+#
+#
+# actor1 = Actor("Ryan", "Gosling", 40)
+# actor2 = Actor("Patrick", "Bateman", 45)
+#
+# Actor.best_one(actor1)
+
+
+string_ = "python"
+dic = {}
+
+for cr in string_:
+
+    if cr not in dic:
+        dic[cr] = 1
+
+    else:
+        dic[cr] += 1
+
+print(dic)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
