15c1de5bec8f598ca4623c1c2cafcf603f4cb893
Dejurniy <gamesrandom68@gmail.com>
2022-11-12 11:34:50 +0400

    oop.homework6

--- oop.homework6.py
+++ oop.homework6.py
@@ -1,9 +1,10 @@
 import json
+import yaml
 
 # Ex. 1
 
 with open("sample.json", "r") as js_file:
-    text = js_file.read()
+    text = json.load(js_file)
 
 
 with open("user_js.txt", "w") as user_js:
@@ -13,27 +14,26 @@ with open("user_js.txt", "w") as user_js:
 # Ex. 2
 
 with open("sample.json", "r") as js_file:
-    text = js_file.read()
+    text = json.load(js_file)
 
 with open("user.yaml", "w") as user_yaml:
-    user_yaml.write(text)
+    yaml.safe_dump(text, user_yaml)
 
 # Ex. 3
 
 with open("user.yaml") as y_file:
-    text = y_file.read()
+    text = yaml.safe_load(y_file)
 
 with open("user_2.json", "w") as js_file2:
-    js_file2.write(text)
+    json.dump(text, js_file2, indent=4)
 
 # Ex. 4
 
 with open("user.yaml") as y_file:
-    text = y_file.read()
+    text = yaml.safe_load(y_file)
 
 with open("user_yaml.txt", "w") as yml_txt:
     yml_txt.write(text)
-
 
 
 
